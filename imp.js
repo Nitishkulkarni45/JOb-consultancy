6e4cf1602d4a125965b994bedf5aed9d9b0274ad
BhushuInamdar <inambhush13@gmail.com>
2023-04-13 13:00:51 +0530

    updated code

--- imp.js
+++ imp.js
@@ -29,8 +29,8 @@ const searchFun = () => {
     let myTable = document.getElementById('excel_data');
     let tr = myTable.getElementsByTagName('tr');
     for (var i = 0; i < tr.length; i++) {
-        let td1 = tr[i].getElementsByTagName('td')[1];
-        let td2 = tr[i].getElementsByTagName('td')[2];
+        let td1 = tr[i].getElementsByTagName('td')[2];
+        let td2 = tr[i].getElementsByTagName('td')[3];
         if (td1) {
             let textvalue1 = td1.textContent || td1.innerHTML;
             if (textvalue1.toUpperCase().indexOf(filter) > -1) {
@@ -46,6 +46,26 @@ const searchFun = () => {
                 }
                 //    git add . then git commit -m "message" then git push origin master
             }
+        }
+    }
+}
+
+const searchFunimp = () => {
+    let filter = document.getElementById('myInputimp').value.toUpperCase();
+    let myTable = document.getElementById('excel_data');
+    let tr = myTable.getElementsByTagName('tr');
+    for (var i = 0; i < tr.length; i++) {
+        let td3 = tr[i].getElementsByTagName('td')[7];
+        if (td3) {
+            let textvalue1 = td3.textContent || td3.innerHTML;
+            if (textvalue1.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            }
+            else {
+                 tr[i].style.display = "none";
+            }
+                //    git add . then git commit -m "message" then git push origin master
+            
         }
     }
 }
